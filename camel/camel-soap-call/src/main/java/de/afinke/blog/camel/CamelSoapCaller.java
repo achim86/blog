2dd8273cd5f2ce63323af829f9cdf97f5353d8f7
Achim Finke <achim.finke@gmail.com>
2016-06-24 09:27:26 +0200

    Initial commit Camel SOAP sample.

---
+++ camel/camel-soap-call/src/main/java/de/afinke/blog/camel/CamelSoapCaller.java
@@ -0,0 +1,12 @@
+package de.afinke.blog.camel;
+
+import org.apache.camel.spring.Main;
+
+public class CamelSoapCaller extends Main {
+
+    public static void main(String[] args) throws Exception {
+        CamelSoapCaller main = new CamelSoapCaller();
+        main.run(args);
+    }
+
+}
