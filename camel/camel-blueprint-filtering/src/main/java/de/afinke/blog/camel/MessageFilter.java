d09fe67cc7d2416e9032646db758deece8d1618d
Achim Finke <achim.finke@gmail.com>
2016-09-15 14:33:47 +0200

    Updated sample.

--- camel/camel-blueprint-filtering/src/main/java/de/afinke/blog/camel/MessageFilter.java
+++ camel/camel-blueprint-filtering/src/main/java/de/afinke/blog/camel/MessageFilter.java
@@ -1,23 +1,30 @@
 package de.afinke.blog.camel;
 
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedService;
+
 import java.util.Arrays;
+import java.util.Dictionary;
 import java.util.List;
 
-public class MessageFilter {
+public class MessageFilter implements ManagedService {
 
-    private List<String> countries;
+    private List<String> countries = Arrays.asList("GER", "LUX");
 
     public String routeTo(String country) {
         if (countries.contains(country)) {
-            return "direct:output";
+            return "vm:output";
         } else {
-            return "direct:trash";
+            return "vm:trash";
         }
 
     }
 
-    public void setCountries(String countries) {
-        this.countries = Arrays.asList(countries.split(","));
+    public void updated(Dictionary<String, ?> dictionary) throws ConfigurationException {
+        if(null == dictionary) {
+            return;
+        }
+        countries = Arrays.asList(String.valueOf(dictionary.get("countries")).replaceAll("\\s+", "").split(","));
     }
 
 }
